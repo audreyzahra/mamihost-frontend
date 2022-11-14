4c32f0bef73175d7085fae85fee408fede59d7c4
Audrey <audreyzahra2@gmail.com>
2022-11-14 11:44:30 +0700

    Fix Packages Host

--- src/pages/Packages/PackagesHost.jsx
+++ src/pages/Packages/PackagesHost.jsx
@@ -113,7 +113,7 @@ const PackagesHost = ({ packageId }) => {
 
         if (token) {
             const data = {
-                "user_email": token.user.email,
+                "user_email": token?.user.email,
                 "duration": durationsConvert(durations),
                 "service_type": serviceType,
                 "service_image": dockerImage,
@@ -122,7 +122,7 @@ const PackagesHost = ({ packageId }) => {
 
             axios.post(`${API_URL}/service/createService`, data, {
                 headers: {
-                    'Authorization': `Bearer ${token.accessToken}`
+                    'Authorization': `Bearer ${token?.accessToken}`
                 }
             })
                 .then((response) => {
@@ -163,52 +163,52 @@ const PackagesHost = ({ packageId }) => {
                 <div className='flex w-2/3'>
                     {hostingList.map((e) => {
                         return (
-                            <div key={e.package_id} className='md:flex-1 rounded-lg m-5'>
-                                {e.package_id === hostDetail.package_id ?
-                                    <Link onSelect={() => PickHost(e.package_name)} className='flex flex-col bg-[#C7DAD4] shadow-md hover:shadow-xl focus:bg-[#C7DAD4] rounded-lg overflow-hidden'>
+                            <div key={e?.package_id} className='md:flex-1 rounded-lg m-5'>
+                                {e?.package_id === hostDetail.package_id ?
+                                    <Link onSelect={() => PickHost(e?.package_name)} className='flex flex-col bg-[#C7DAD4] shadow-md hover:shadow-xl focus:bg-[#C7DAD4] rounded-lg overflow-hidden'>
                                         <div className='p-4 pb-24'>
                                             <div className='h-1/6'>
-                                                <h2 className='font-bold text-[#2F414F] text-center mt-2'>{e.package_name}</h2>
+                                                <h2 className='font-bold text-[#2F414F] text-center mt-2'>{e?.package_name}</h2>
                                             </div>
                                             <hr className="m-3" />
                                             <div className="flex justify-start items-center gap-2">
-                                                {featureIcon(e.features.storage)}{e.features.storage} MB
+                                                {featureIcon(e?.features.storage)}{e?.features.storage} MB
                                             </div>
                                             <div className="flex justify-start items-center gap-2">
-                                                {featureIcon(e.features.unlimited_bandwidth)}{e.features.unlimited_bandwidth ? 'Unlimited Bandwidth' : 'No Unlimited Bandwidth'}
+                                                {featureIcon(e?.features.unlimited_bandwidth)}{e?.features.unlimited_bandwidth ? 'Unlimited Bandwidth' : 'No Unlimited Bandwidth'}
                                             </div>
                                             <div className="flex justify-start items-center gap-2">
-                                                {featureIcon(e.features.support_domain)}{e.features.support_domain ? 'Free Domain' : 'No Free Domain'}
+                                                {featureIcon(e?.features.support_domain)}{e?.features.support_domain ? 'Free Domain' : 'No Free Domain'}
                                             </div>
                                             <div className="flex justify-start items-center gap-2">
-                                                {featureIcon(e.features.support_SSD)}{e.features.support_SSD ? 'Free SSD' : 'No Free SSD'}
+                                                {featureIcon(e?.features.support_SSD)}{e?.features.support_SSD ? 'Free SSD' : 'No Free SSD'}
                                             </div>
                                             <div className="flex justify-start items-center gap-2">
-                                                {featureIcon(e.features.support_SSL)}{e.features.support_SSL ? 'Free SSL' : 'No Free SSL'}
+                                                {featureIcon(e?.features.support_SSL)}{e?.features.support_SSL ? 'Free SSL' : 'No Free SSL'}
                                             </div>
                                         </div>
                                     </Link>
                                     :
-                                    <Link onSelect={() => PickHost(e.package_name)} className='flex flex-col bg-white shadow-md hover:shadow-xl focus:bg-[#C7DAD4] rounded-lg overflow-hidden'>
+                                    <Link onSelect={() => PickHost(e?.package_name)} className='flex flex-col bg-white shadow-md hover:shadow-xl focus:bg-[#C7DAD4] rounded-lg overflow-hidden'>
                                         <div className='p-4 pb-24'>
                                             <div className='h-1/6'>
-                                                <h2 className='font-bold text-[#2F414F] text-center mt-2'>{e.package_name}</h2>
+                                                <h2 className='font-bold text-[#2F414F] text-center mt-2'>{e?.package_name}</h2>
                                             </div>
                                             <hr className="m-3" />
                                             <div className="flex justify-start items-center gap-2">
-                                                {featureIcon(e.features.storage)}{e.features.storage} MB
+                                                {featureIcon(e?.features.storage)}{e?.features.storage} MB
                                             </div>
                                             <div className="flex justify-start items-center gap-2">
-                                                {featureIcon(e.features.unlimited_bandwidth)}{e.features.unlimited_bandwidth ? 'Unlimited Bandwidth' : 'No Unlimited Bandwidth'}
+                                                {featureIcon(e?.features.unlimited_bandwidth)}{e?.features.unlimited_bandwidth ? 'Unlimited Bandwidth' : 'No Unlimited Bandwidth'}
                                             </div>
                                             <div className="flex justify-start items-center gap-2">
-                                                {featureIcon(e.features.support_domain)}{e.features.support_domain ? 'Free Domain' : 'No Free Domain'}
+                                                {featureIcon(e?.features.support_domain)}{e?.features.support_domain ? 'Free Domain' : 'No Free Domain'}
                                             </div>
                                             <div className="flex justify-start items-center gap-2">
-                                                {featureIcon(e.features.support_SSD)}{e.features.support_SSD ? 'Free SSD' : 'No Free SSD'}
+                                                {featureIcon(e?.features.support_SSD)}{e?.features.support_SSD ? 'Free SSD' : 'No Free SSD'}
                                             </div>
                                             <div className="flex justify-start items-center gap-2">
-                                                {featureIcon(e.features.support_SSL)}{e.features.support_SSL ? 'Free SSL' : 'No Free SSL'}
+                                                {featureIcon(e?.features.support_SSL)}{e?.features.support_SSL ? 'Free SSL' : 'No Free SSL'}
                                             </div>
                                         </div>
                                     </Link>}
@@ -363,15 +363,15 @@ const PackagesHost = ({ packageId }) => {
                 <div className="flex w-2/3">
                     {duration.map((e) => {
                         return (
-                            <div key={e.duration_id} className='md:flex-1 rounded-lg m-5'>
-                                <Link onClick={event => PickPrice(event, e.name, e.total_price)} className='flex flex-col bg-white shadow-md hover:shadow-xl focus:bg-[#C7DAD4] rounded-lg overflow-hidden'>
+                            <div key={e?.duration_id} className='md:flex-1 rounded-lg m-5'>
+                                <Link onClick={event => PickPrice(event, e?.name, e?.total_price)} className='flex flex-col bg-white shadow-md hover:shadow-xl focus:bg-[#C7DAD4] rounded-lg overflow-hidden'>
                                     <div className='h-1/3 bg-[#FFC210] p-3'>
-                                        <h2 className='font-bold text-white text-center mt-2'>{e.name}</h2>
+                                        <h2 className='font-bold text-white text-center mt-2'>{e?.name}</h2>
                                     </div>
                                     <div className="h-2/3 text-center p-3">
-                                        <p className="font-medium p-2">{e.price}</p>
+                                        <p className="font-medium p-2">{e?.price}</p>
                                         <p className="text-xs text-gray-400 font-medium pt-2">Total</p>
-                                        <p className="font-medium p-2 text-teal-700">{e.total_price}</p>
+                                        <p className="font-medium p-2 text-teal-700">{e?.total_price}</p>
                                     </div>
                                 </Link>
                             </div>
@@ -442,7 +442,7 @@ const PackagesHost = ({ packageId }) => {
                                 <div className="p-7 text-center justify-center items-center align-center">
                                     <CheckCircleIcon className="mx-auto justify-center fill-green-500 h-20" />
                                     <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Hosting Success !</h3>
-                                    <Link to={`/dashboard/${token.user.email}`} className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                    <Link to={`/dashboard/${token?.user.email}`} className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
                                         Kembali ke Dashboard
                                     </Link>
                                     <button onClick={() => setModalSuccess(false)} type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Keluar</button>
